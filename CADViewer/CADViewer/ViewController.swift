9e57e12f049ef9c9b5e95448656d3b110ac7f886
James Borthwick <jborthwick@pdftron.com>
2020-10-08 21:11:48 -0700

    Empty viewer; project stuff
    Signed-off-by: James Borthwick <jborthwick@pdftron.com>

--- CADViewer/CADViewer/ViewController.swift
+++ CADViewer/CADViewer/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import PDFNet
+import Tools
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
         // Do any additional setup after loading the view.
+        super.viewDidAppear(animated)
+        
+        let pdfController = PTDocumentController()
+        
+        let navController = UINavigationController(rootViewController: pdfController)
+        navController.navigationBar.isTranslucent = false
+        navController.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+        self.present(navController, animated: true, completion: nil)
+        
     }
 
 
