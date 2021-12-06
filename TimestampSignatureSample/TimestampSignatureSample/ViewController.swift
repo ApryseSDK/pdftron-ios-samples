a5b924683f5f9ba55829812705d6e2d19eee376b
Yifan Zhang <49728023+OoaLH@users.noreply.github.com>
2021-12-06 14:17:18 -0800

    Add new sample project.

---
+++ TimestampSignatureSample/TimestampSignatureSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimestampSignatureSample
+//
+//  Created by Yifan Zhang on 2021-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
