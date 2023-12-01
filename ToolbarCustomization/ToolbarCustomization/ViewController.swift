94203a0a52d32d73059c8620098bc17370c9f5ea
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2023-12-01 11:48:35 -0700

    Initial commit of ToolbarCustomization sample

---
+++ ToolbarCustomization/ToolbarCustomization/ViewController.swift
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------------------
+// Copyright (c) 2001-2022 by PDFTron Systems Inc. All Rights Reserved.
+// Consult legal.txt regarding legal and license information.
+//---------------------------------------------------------------------------------------
+
+import UIKit
+import Tools
+
+class ViewController: UIViewController {
+
+    // https://docs.apryse.com/documentation/ios/get-started/viewdoc/
+    override func viewDidAppear(_ animated: Bool) {
+        // Create a CustomDocumentController
+        let documentController = PTDocumentController()
+        // The PTDocumentController must be in a navigation controller before a document can be opened
+        let navigationController = UINavigationController(rootViewController: documentController)
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.toolbar.isTranslucent = false
+        // Open a file from URL.
+        let fileURL: URL = URL(string:"https://pdftron.s3.amazonaws.com/downloads/pl/sample.pdf")!
+        documentController.openDocument(with: fileURL)
+        // Show navigation (and document) controller.
+        self.present(navigationController, animated: true, completion: nil)
+    }
+
+
+}
+
