ace5c6c2d2a585f0dce063607c1c2fe731a76ca7
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2023-12-29 11:13:08 -0700

    Add Apryse SDK and show a document

--- ApryseComponentSubclassing/ApryseComponentSubclassing/ViewController.swift
+++ ApryseComponentSubclassing/ApryseComponentSubclassing/ViewController.swift
@@ -4,6 +4,7 @@
 //---------------------------------------------------------------------------------------
 
 import UIKit
+import Tools
 
 class ViewController: UIViewController {
 
@@ -11,7 +12,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Create a PTDocumentController
+        let documentController = PTDocumentController()
+        // The PTDocumentController must be in a navigation controller before a document can be opened
+        let navigationController = UINavigationController(rootViewController: documentController)
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.toolbar.isTranslucent = false
+        // Open a file from URL.
+        let fileURL: URL = URL(string:"https://pdftron.s3.amazonaws.com/downloads/pl/sample.pdf")!
+        documentController.openDocument(with: fileURL)
+        // Show navigation (and document) controller.
+        self.present(navigationController, animated: true, completion: nil)
+    }
 }
 
