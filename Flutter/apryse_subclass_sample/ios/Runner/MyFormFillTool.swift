78a61f853f2691baa84c72e4ac9c617817116635
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2024-03-12 11:54:00 -0600

    Add MyFormFillTool file and increase minimum iOS deployment version to 16

---
+++ Flutter/apryse_subclass_sample/ios/Runner/MyFormFillTool.swift
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------------------
+// Copyright (c) 2001-2022 by PDFTron Systems Inc. All Rights Reserved.
+// Consult legal.txt regarding legal and license information.
+//---------------------------------------------------------------------------------------
+
+import UIKit
+
+class MyFormFIllTool: PTFormFillTool {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
