71641d7997f081fc522bcc95d53a4dc6b0dca666
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2024-04-01 14:46:05 -0600

    Initial commit and remove storyboard

---
+++ ExtractAnnotatedPages/ExtractAnnotatedPages/ViewController.swift
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------------------
+// Copyright (c) 2001-2022 by PDFTron Systems Inc. All Rights Reserved.
+// Consult legal.txt regarding legal and license information.
+//---------------------------------------------------------------------------------------
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
