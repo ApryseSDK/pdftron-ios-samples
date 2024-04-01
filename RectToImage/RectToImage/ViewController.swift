34b97dcdb670ee5ddb68e69a1fd888ba84e137ca
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2024-04-01 12:38:04 -0600

    Initial commit and remove storyboard

---
+++ RectToImage/RectToImage/ViewController.swift
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------------------
+// Copyright (c) 2001-2022 by PDFTron Systems Inc. All Rights Reserved.
+// Consult legal.txt regarding legal and license information.
+//---------------------------------------------------------------------------------------
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
